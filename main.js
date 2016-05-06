639d6e792151ee63243f7a9c2952ad25c52e924b
KC Baeten <kcbaeten@KCs-MacBook-Pro.local>
2016-05-06 11:17:30 -0600

    Presentation Status

--- main.js
+++ main.js
@@ -2,13 +2,66 @@ var networkKey= "key=5eed18d0e03d592433311884813bce77";
 var searchUrl
 var styleId
 var abvPerc
+var nameList = []
+
+
+$("#search").on("click", function(e){
+  e.stopPropagation();
+  if ($('.storageUl').length > 0) return;
+  var list = localStorage.getItem('nameList');
+
+  if(!list) return;
+  list = JSON.parse(list);
+
+  var storageList= $("<p><class='.storageUl'></p>");
+
+  for (var i = 0; i < list.length; i++) {
+    var li = $('<p>' + "Last Searched: " + list[i] + '</p>');
+    storageList.append(li)
+  }
+  $(".storage").append(storageList);
+});
+
+$('input').on("click", function(){
+  for (var i = 0; i < $(".style").length; i++) {
+    if($(".style")[i].checked === true) {
+      var name = $(".style")[i].value;
+    }
+  }
+    console.log(name);
+var list = localStorage.getItem('nameList');
+if(!list) {
+  list = [];
+  list.push(name);
+}
+else{
+  var listArray = JSON.parse(list);
+  if(listArray.indexOf(name) === -1) {
+    listArray[0]= name;
+
+  }
+  list = listArray;
+}
+localStorage.setItem('nameList', JSON.stringify(list));
+console.log(localStorage.getItem("nameList"));
+
+var list = localStorage.getItem('nameList');
+if(!list) return;
+list = JSON.parse(list);
+$('.storageUl').remove();
+});
+
+
 
 
 function clear() {
   $("#cards").empty();
+  // $('.storage').empty();
 }
 
-$("#clear").on("click", clear)
+
+
+$("#clear").on("click", clear);
 
 $("#warning").hide();
 
@@ -16,13 +69,13 @@ function beerSearch () {
   var checkCount = 0;
   var abvCount = 0;
   $("#search").on("click", function(){
+    clear();
     for (var i = 0; i < $(".style").length; i++) {
       if($(".style")[i].checked === true) {
         styleId= $(".style")[i].id;
       }
       else if ($(".style")[i].checked === false){
         checkCount++
-        console.log(checkCount);
       }
     }
     for (var j = 0; j < $(".abv").length; j++) {
@@ -31,7 +84,6 @@ function beerSearch () {
       }
       else if ($(".abv")[j].checked === false){
         abvCount++
-        console.log(abvCount);
       }
     }
     if (checkCount === 12 && abvCount === 5) {
